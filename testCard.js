391000902b148da30b0b23c97ebff00b12b1b4d6
1Raj <betterintro+rajfred@gmail.com>
2020-10-16 10:45:15 +1100

    test card 200 size

--- testCard.js
+++ testCard.js
@@ -1,29 +1,43 @@
-const icon = '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"></circle>'
+const icon = '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="4"></circle>'
 
 miro.onReady(() => {
   miro.initialize({
     extensionPoints: {
       bottomBar: {
-        title: 'Board cleaner Raj',
+        title: 'Add test Card Raj title 10:44',
         svgIcon: icon,
         onClick: async () => {
 
           // Show modal and wait for user choice
-          let needToClear = confirm('Hey! This will delete all content. Do you want delete all content?')
+          let needToClear = confirm('Hey! This will add card?')
 
           if (needToClear) {
             // Get all board objects
-            let objects = await miro.board.widgets.get()
+            //let objects = await miro.board.widgets.get()
 
             // Delete all board objects
-            await miro.board.widgets.deleteById(objects.map(object => object.id))
+            //await miro.board.widgets.deleteById(objects.map(object => object.id))
+
+            // Create new card
+            await miro.board.widgets.create
+            (
+              {
+                "type":'card', 
+                  "title": "Story Title",
+                  "description": "Story Description",
+                  "bounds": {
+                      "width": 200,
+                      "height": 60
+                  }
+              }
+            )
 
             // Display success
-            miro.showNotification('Content has been deleted')
+            miro.showNotification('Card has been added')
           }
           else
           {
-            miro.showNotification('Alright, canceled the delete action')
+            miro.showNotification('Alright, canceled the action')
           }
         }
       }
