4320116c0326f195d05f3fa7ba86e73c3e992ece
rajfred <32629904+rajfred@users.noreply.github.com>
2020-10-15 16:56:06 +1100

    Update addTestCard.js

--- addTestCard.js
+++ addTestCard.js
@@ -16,10 +16,40 @@ miro.onReady(() => {
             //let objects = await miro.board.widgets.get()
 
             // Create new card
-          await miro.board.widgets.create({type: 'card',                                           
-                                          "title": "AzDo out Story title",
-                                          "description": "Details if any..."                                          
-                                          })
+          await miro.board.widgets.create
+          (
+            {
+                "type": 'CARD',
+                "bounds": {
+                    "width": 100,
+                    "height": 60
+                },
+                "x": 100,
+                "y": 100,
+                "scale": 100,
+                "style": {
+                    "backgroundColor": #608bd1
+                },
+                "title": "Story Title",
+                "description": "Story Description",                
+                "card": {
+                         "customFields": [
+                             {"value": "Custom Value 1"},
+                             ]
+                         ,
+                        "logo":{iconUrl: ""} //maybe subject to CORS restrictions
+                   }
+                 ,
+                "metadata": {
+                    [miroAppClientId]: {
+                        "key 1": "value 1",
+                        "key 2": "value 2",
+                        "key 3": "value 3",
+                    }
+                }
+                                          
+            }
+          )
 
             // Display success
             miro.showNotification('Card has been added')
