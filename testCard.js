08240901afbc07d13e435829f950f53fb12a150c
1Raj <betterintro+rajfred@gmail.com>
2020-10-16 10:39:17 +1100

    del check

--- testCard.js
+++ testCard.js
@@ -1,43 +1,32 @@
 const icon = '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"></circle>'
 
 miro.onReady(() => {
-    miro.initialize({
-      extensionPoints: {
-        bottomBar: {
-          title: 'Add Card Rect 5:55',
-          svgIcon: icon,
-          onClick: async () => {
-  
-            // Show modal and wait for user choice
-            let blnAddCard = confirm('Hey! This will add a card? 3')
-  
-            if (blnAddCard) {
-              // Get all board objects
-              //let objects = await miro.board.widgets.get()
-  
-              // Create new card
-            await miro.board.widgets.create
-            (
-              {
-                "type":'card', 
-                  "title": "Story Title",
-                  "description": "Story Description",
-                  "bounds": {
-                      "width": 200,
-                      "height": 60
-                  }
-              }
-            )
-  
-              // Display success
-              miro.showNotification('Card has been added')
-            }
-            else
-            {
-              miro.showNotification('Alright, not adding a card')
-            }
+  miro.initialize({
+    extensionPoints: {
+      bottomBar: {
+        title: 'Board cleaner Raj',
+        svgIcon: icon,
+        onClick: async () => {
+
+          // Show modal and wait for user choice
+          let needToClear = confirm('Hey! This will delete all content. Do you want delete all content?')
+
+          if (needToClear) {
+            // Get all board objects
+            let objects = await miro.board.widgets.get()
+
+            // Delete all board objects
+            await miro.board.widgets.deleteById(objects.map(object => object.id))
+
+            // Display success
+            miro.showNotification('Content has been deleted')
+          }
+          else
+          {
+            miro.showNotification('Alright, canceled the delete action')
           }
         }
       }
-    })
+    }
   })
+})
