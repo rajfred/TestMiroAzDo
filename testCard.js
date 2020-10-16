b4177397bc2a96a4332f04da6b72528c976a2144
1Raj <betterintro+rajfred@gmail.com>
2020-10-16 23:07:31 +1100

    parse JSON

--- testCard.js
+++ testCard.js
@@ -4,12 +4,13 @@
 //const { default: Axios } = require("axios")
 
 const icon = '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="4"></circle>'
+var responsedata;
 
 miro.onReady(() => {
   miro.initialize({
     extensionPoints: {
       bottomBar: {
-        title: 'Raj: Card npm axios response retry 10:26',
+        title: 'Raj: Card npm response parse 11:07',
         svgIcon: icon,
         onClick: async () => {
 
@@ -40,9 +41,12 @@ miro.onReady(() => {
 
             var request = new XMLHttpRequest()
 
+            
+
             request.open('GET', 'https://dev.azure.com/TeamFred/_apis/wit/workItems/98498', true)
             request.setRequestHeader('Content-Type', 'application/json; charset=utf-8;');
             request.setRequestHeader ("Authorization", "Basic " + btoa('Basic' + ":" + 'cxdfzxfkdrpfdcurbfb22i6k3m5esccwfpimzsglusosxe4krivq'));
+            request.responseType = 'json';
 
             request.onload = function() {
               // Begin accessing JSON data here
@@ -50,7 +54,9 @@ miro.onReady(() => {
               var data = JSON.parse(this.response)
 
               if (request.status >= 200 && request.status < 400) {
-                console.log(data)
+                  console.log(data);
+                  responsedata = JSON.parse(data);
+                  console.log(responsedata);
                 } else {
                 console.log('error')
                   }
@@ -58,14 +64,24 @@ miro.onReady(() => {
 
             request.send()
 
+            /*
+            Microsoft.VSTS.Scheduling.Effort: 2
+            System.AreaPath: "Pharmacy\Program\Dracarys"
+            System.AssignedTo: {displayName: "Pradeep Vajrapu"
+            System.Description: "<div>When a pharmacist dispenses a script th
+            System.IterationPath: "Pharmacy\PI 7\Sprint 1"
+            System.Title: "V2 scripts must be initiated via Flow"
+            System.WorkItemType: "Product Backlog Item"
+            */
+
+
             // Create new card
             await miro.board.widgets.create
             (
               {
                 "type":'card', 
                   "title": "Story Title",
-                  "description": "Some discription"
-                  
+                  "description": responsedata.fields.System.Description                  
               }
             )
 
