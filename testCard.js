7ad8796ef20e678ca90698ec51d55515671ae0a7
1Raj <betterintro+rajfred@gmail.com>
2020-10-16 13:56:42 +1100

    added axios

--- testCard.js
+++ testCard.js
@@ -1,3 +1,5 @@
+require('dotenv-safe').config()
+const axios = require('axios')
 
 const icon = '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="4"></circle>'
 const viewport = await miro.board.viewport.get()
