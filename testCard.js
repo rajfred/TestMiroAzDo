9aaea24764700fc4e621d0754744dc41aaa14818
1Raj <betterintro+rajfred@gmail.com>
2020-10-16 14:28:54 +1100

    removed dotenv

--- testCard.js
+++ testCard.js
@@ -1,4 +1,4 @@
-require('dotenv-safe').config()
+//require('dotenv-safe').config()
 const axios = require('axios')
 
 const icon = '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="4"></circle>'
