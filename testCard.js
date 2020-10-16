eba6fbe2699f11415253d8baa8bcca7b5f0b3850
1Raj <betterintro+rajfred@gmail.com>
2020-10-16 14:43:39 +1100

    removed axios from js added to html

--- testCard.js
+++ testCard.js
@@ -1,7 +1,5 @@
 //require('dotenv-safe').config()
 //const axios = require('axios')
-<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
-
 
 const icon = '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="4"></circle>'
 
