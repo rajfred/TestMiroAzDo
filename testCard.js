0d45033b80efa28084dde07672bc11cdac92d745
1Raj <betterintro+rajfred@gmail.com>
2020-10-19 09:23:31 +1100

    config env require

--- testCard.js
+++ testCard.js
@@ -1,13 +1,12 @@
 //require('dotenv-safe').config()
 //const axios = require('axios')
+require('dotenv').config();  
+
 
 //const { default: Axios } = require("axios")
 
 const icon = '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="4"></circle>'
 var responsedata;
-
-
-
 
 miro.onReady(() => {
   miro.initialize({
