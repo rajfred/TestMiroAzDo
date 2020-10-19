2829df7e8c2cc01c7996be775ff876f1a9de9055
1Raj <betterintro+rajfred@gmail.com>
2020-10-19 14:24:02 +1100

    this.response

--- testCard.js
+++ testCard.js
@@ -68,7 +68,7 @@ miro.onReady(() => {
 
             request.onload = function() {
               // Begin accessing JSON data here
-
+              console.log(this.response)
                //data = JSON.parse(this.response)
 
               if (request.status >= 200 && request.status < 400) {
